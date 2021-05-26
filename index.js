16a533983720147c6e67be296810840f902a1ae3
Nathan Huse <nathan.huse@yahoo.com>
2021-05-26 18:18:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function takeANumber(currentLine, newName) {
 }
 
 function nowServing(currentLine) {
-  if(currentLine.length==0) {
+  if(currentLine.length=0) {
     return 'There is nobody waiting to be served!';
   }
-  return `Currently serving ${currentLine.shift()}`;
+  return `Currently serving ${currentLine.shift()}.`;
 }
 
 function currentLine(currentLine) {
